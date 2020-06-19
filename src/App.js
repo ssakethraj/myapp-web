e9a635fc01e7e62b9c94b56bcf28c12e88026f35
ssakethraj <ssakethraj@gmail.com>
2020-06-19 16:55:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     name: ""
+  //   }
+  // }
+  state = {
+    products: []
+  }
+  componentDidMount() {
+    // fetch("http://localhost:3000/").then(resp => {
+    //   return resp.json()
+    // }).then(data => {
+    //   console.log(data)
+    // })
+
+    axios.get("http://localhost:3000/").then(res => {
+      this.setState({
+        products: res.data
+      })
+      console.log(res.data);
+    })
+  }
+  render(){
+    const {products} = this.state;
+    return (
+      <div className="App">
+        <ul>
+          {
+            products.map((item, index) => {
+              return (<li>
+                <h2>{item.name}</h2>
+                <p>{item.desc}</p>
+              </li>)
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
